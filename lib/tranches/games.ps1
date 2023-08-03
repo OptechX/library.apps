7020cbe29517e89f9df7636e2a54f5dce25a71ee
danijeljw-rpc <danijel@repasscloud.com>
2023-08-03 22:34:38 +1000

    test cat:GAMES (#66)

--- lib/tranches/games.ps1
+++ lib/tranches/games.ps1
@@ -7,7 +7,7 @@
 . (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/do-not-edit-below-this-line.ps1)
 
 # set category Games
-$Env:applicationCategory = "Games"
+$Env:applicationCategory = "Games".ToUpper()
 $ps1_files = Get-ChildItem -Path $Env:applicationCategory -Recurse -Filter "*.ps1"
 Write-Output "Matched $($ps1_files.Length) manifests"
 foreach ($ps1_file in $ps1_files)
