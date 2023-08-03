2a97980f65db204e5712bfb65cd076a212fc02e5
Danijel-James Wynyard <danijel@repasscloud.com>
2023-08-03 22:43:32 +1000

    debug

--- lib/tranches/games.ps1
+++ lib/tranches/games.ps1
@@ -9,7 +9,8 @@
 # set category Games
 $Env:applicationCategory = "Games".ToUpper()
 $path = Join-Path -Path (Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent) -ChildPath $Env:applicationCategory
-Write-Output $path
+Write-Output "Show the directory"
+Get-ChildItem -Path $path -Recurse
 $ps1_files = Get-ChildItem -Path $path -Recurse -Filter "*.ps1"
 Write-Output "Matched $($ps1_files.Length) manifests"
 foreach ($ps1_file in $ps1_files)
