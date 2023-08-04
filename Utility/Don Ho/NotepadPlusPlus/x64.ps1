ddb46aa878744631cd60853fdefaa31d57a68571
Danijel-James Wynyard <danijel@repasscloud.com>
2023-08-05 01:36:19 +1000

    ad utility

--- Utility/Don Ho/NotepadPlusPlus/x64.ps1
+++ Utility/Don Ho/NotepadPlusPlus/x64.ps1
@@ -1,27 +1,25 @@
 <# Manifest Version Info #>
-$ManifestVersion='6.6'
-$LastUpdate='2023-01-27'
+$ManifestVersion='6.8-Utility'
+$LastUpdate='2023-08-04'
 Write-Output "Manifest Version: ${ManifestVersion}"
 Write-Output "Last Updated: ${LastUpdate}"
 
-
 <# Create static new object #>
 $new_app = [applicationPayload]::new()
-$new_app.Category = $Env:applicationCategory.Replace(" ","").ToUpper()
+$new_app.Category = $Env:applicationCategory.Replace(" ","").ToUpper().Replace(' ','').ToUpper()
 $new_app.Publisher = "Don Ho"
 $new_app.Name = "Notepad++"
 $new_app.Lcid = @("EN_US")
 $new_app.CpuArch = @("x64")
 $new_app.Homepage = "https://notepad-plus-plus.org/"
 $new_app.Copyright = "Copyright (C) 2021 Don Ho"
-$new_app.Icon = "https://github.com/OptechX/library.apps.images/raw/main/$($Env:applicationCategory)/Don%20Ho/Notepad%2B%2B/icon.png"
+$new_app.Icon = "https://github.com/OptechX/library.apps.images/raw/main/$($Env:applicationCategory)/Don%20Ho/Notepad%2B%2B/icon.png".Replace(" ","%20")
 $new_app.LicenseAccept = $false
 $new_app.Docs = "https://notepad-plus-plus.org/online-help/"
 $new_app.License = "https://github.com/notepad-plus-plus/notepad-plus-plus/blob/master/LICENSE"
 $new_app.Tags = @("notepad","notepadplusplus","notepad-plus-plus","plus","notepad-plus","notepad++","development","text","editor")
 $new_app.Summary = "Notepad++ is a free (as in `"free speech`" and also as in `"free beer`") source code editor and Notepad replacement that supports several languages"
 $new_app.RebootRequired = $false
-
 
 <# Get icon.png if not already obtained #>
 $icon_path_svg = "${PSScriptRoot}/icon.svg"
@@ -42,7 +40,6 @@ else
 {
 
 }
-
 
 <# Get app data #>
 if ($new_app.GithubUrl -ne [string]::Empty)
@@ -66,6 +63,23 @@ else
     $new_app.Executable = 'exe'
 }
 
-
 <# ============== DO NOT EDIT BELOW THIS LINE ============== #>
-Invoke-DoNotEditBelowThisLine -InputPayload $new_app
+$new_pkg = [applicationPackage]::new()
+$new_pkg.uid = "$($new_app.Publisher -replace "[^a-zA-Z0-9]")::$($new_app.Name -replace "[^a-zA-Z0-9]")"
+$new_pkg.lastUpdate = Get-Date
+$new_pkg.applicationCategory = $new_app.Category
+$new_pkg.publisher = $new_app.Publisher
+$new_pkg.name = $new_app.Name
+$new_pkg.version = $new_app.Version
+$new_pkg.copyright = $new_app.Copyright
+$new_pkg.licenseAcceptRequired = $new_app.LicenseAccept
+$new_pkg.lcid = $new_app.Lcid
+$new_pkg.cpuArch = $new_app.CpuArch
+$new_pkg.homepage = $new_app.Homepage
+$new_pkg.icon = $new_app.Icon
+$new_pkg.docs = $new_app.Docs
+$new_pkg.license = $new_app.License
+$new_pkg.tags = $new_app.Tags
+$new_pkg.summary = $new_app.Summary
+
+$new_pkg | ConvertTo-Json | Out-File -FilePath "${PSScriptRoot}/$($new_app.CpuArch[0]).json" -Force -Confirm:$false -Encoding utf8
