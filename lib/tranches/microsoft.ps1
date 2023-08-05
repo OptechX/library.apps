8ea9186cbc31c303cda41730d6643cb0fc240a34
Danijel-James Wynyard <danijel@repasscloud.com>
2023-08-05 15:19:11 +1000

    test dotnet

--- lib/tranches/microsoft.ps1
+++ lib/tranches/microsoft.ps1
@@ -4,22 +4,34 @@
 
 # load functions
 . (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/GetGithubReleaseDownload.ps1)
+. (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/GetGithubVersionFromTags.ps1)
+. (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/do-not-edit-below-this-line.ps1)
 . (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/GetRedirectedUrl.ps1)
 . (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/GetMSDotNetPackageDetails.ps1)
 . (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/GetGHDotNetPackageDetails.ps1)
-. (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath functions/do-not-edit-below-this-line.ps1)
 
 # set category Microsoft
 $Env:applicationCategory = "Microsoft"
+
+# clone winget directory
+git clone git@github.com:OptechX/winget-pkgs.git
+
+# install PowerShell-Yaml module
+Install-Module -Name PowerShell-Yaml -Force
+
+# process ps1 files
 $ps1_files = Get-ChildItem -Path $Env:applicationCategory -Recurse -Filter "*.ps1"
 Write-Output "Matched $($ps1_files.Length) manifests"
 foreach ($ps1_file in $ps1_files)
 {
     Write-Output "Starting $($ps1_file.FullName)"
-    try {
-        . $ps1_file.FullName
-    }
-    catch {
-        Write-Output "Error: $($_.Exception)"
-    }
+    . $ps1_file.FullName
+}
+
+# execute update
+$json_files = Get-ChildItem -Path $Env:applicationCategory -Recurse -Filter "*.json" | Where-Object { $_.Name -match 'x86_ingestScript.json|x64_ingestScript.json' }
+foreach ($json_file in $json_files)
+{
+    Write-Output "Starting $($json_file.FullName)"
+    & ./bin/oxlaut --json "${json_file}"
 }
