cf9c42ff35316d5ae0e7761c95cc48050ac59850
danijeljw-rpc <danijel@repasscloud.com>
2023-08-06 00:41:50 +1000

    Auto pr microsoft into dev (#82)
    * app:/Microsoft/IronPython2
    
    * app:Microsoft/IronPython3
    
    * try fix IronPython3
    
    * resolve iron python issue

--- Microsoft/Microsoft.Edge.ps1
+++ Microsoft/Microsoft.Edge.ps1
@@ -2,4 +2,9 @@
 $pRoot = "./winget-pkgs/manifests/m/Microsoft/Edge"
 $versionMajor = Get-ChildItem -Path $pRoot -Directory
 
-Invoke-JsonBuilder -VersionMajor $versionMajor
+try {
+    Invoke-JsonBuilder -VersionMajor $versionMajor
+}
+catch {
+
+}
